68f0eca3550fe9baaed41e2260402ae5d1abcac3
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-08-10 09:42:55 +0200

    Add project files.

---
+++ CSGO/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGO
+{
+    internal class Player
+    {
+        //instansfelter:
+
+        private int id;
+        private string nickName;
+        private int kills;
+        private int deaths;
+
+        //Konstruktør:
+
+        public Player(int Id, string NickName, int Kills, int Deaths)
+        {
+            id = Id;
+            nickName = NickName;
+            kills = Kills;
+            deaths = Deaths;
+        }
+
+        //Properties:
+
+        public int Id { get { return id; } }
+        public string NickName { get { return nickName; } }
+        public int Kills { get { return kills; } }
+
+        public int Deaths { get { return deaths; } }
+
+
+        // ToString() method: 
+
+        public override string ToString()
+        {
+            return $"Players details: ID: {Id}, NickName: {NickName}, Kills: {Kills}, Deaths: {Deaths}";
+        }
+
+    }
+
+}
+
---
+++ CSGO/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSGO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player player1 = new Player(001, "Nuriye", 42, 9);
+            Player player2 = new Player(002, "Gülsüm", 18, 9);
+
+
+            Console.WriteLine(player1);
+            Console.WriteLine(player2);
+        }
+    }
+}
