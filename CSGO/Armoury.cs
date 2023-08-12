43f094601102cc2716d6debc17d1100cea96406e
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-08-12 12:28:31 +0200

    yoo

--- CSGO/Armoury.cs
+++ CSGO/Armoury.cs
@@ -9,14 +9,43 @@ namespace CSGO
     internal class Armoury 
     {
 
-        
+        //Creating a dictionary and adding two weapons: 
         Dictionary<int, Weapon> armouryDictionary = new Dictionary<int, Weapon>();
 
         public Armoury() 
         { 
-        armouryDictionary[1] = "AK-47";
-        armouryDictionary[2] = "M4A1-S";
+
+        Weapon weaponOne = new Weapon("AK-47", 90, 25, 2, 1200, "T");
+        Weapon weaponTwo = new Weapon("M4A1-S", 80, 30, 1, 1100, "CT");
+
+        armouryDictionary[1] = weaponOne;
+        armouryDictionary[2] = weaponTwo;
         }
-         
+
+        public void Create()
+        {
+           
+        }
+
+        public void Read() 
+        {
+
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Delete()
+        {
+            
+        }
+
+        public void PurchaseWeapon()
+        {
+            
+        }
+
     }
 }
